c07454f0a3a65f91f6c4855c239d154d04f4cbbb
Tobias <tbp@kontrapunkt.com>
2020-02-24 11:14:18 +0100

    Added testing for keys for projects and costumers

--- workbook_api_test.py
+++ workbook_api_test.py
@@ -15,10 +15,83 @@ api = WorkbookAPI(
   config.get('workbook', 'password'),
   )
 
+PROJECT_KEYS = set({
+  'ProjectRetainer',
+  'Active',
+  'ProjectName',
+  'ResourceId',
+  'ProjectRetainerAllowDeliveryJobToExceedMasterBudgetWhenClosed',
+  'ProjectRetainerIsMixedRetainer',
+  'ProjectNumber',
+  'ResponsibleResourceId',
+  'Id',
+  'Active',
+  'ProjectFolder',
+  'ProjectRetainerSummarizeActivityLines',
+  'ColorId'
+  #'UpdateResourceId',
+  #'UpdateDate',
+  })
+
+COSTUMER_KEYS = set({
+  'Active',
+  'ActivityGrouping',
+  'AllowInvoiceExceedApprovedPriceQuote',
+  'AllowInvoiceWithoutApprovedPriceQuote',
+  'AllowMultiResource',
+  'AllowPriceQuoteExceedClientOrder',
+  'CompDepartmentId',
+  'CopyPrintSettingsNewPrice',
+  'CurrencyId',
+  #'CustCode1',
+  #'CustCode2',
+  #'CustCode3',
+  #'CustCode4',
+  #'CustCode5',
+  'CustomerAccountNumberRequiredOnInvoice',
+  'CustomerInvoiceApprovalHandlingActivated',
+  'CustomerOrderNumberRequiredOnInvoice',
+  'DeliveryAddress',
+  'EnableInterCompanySettlement',
+  'Id',
+  'InterCompanyTimeRegistrationAllowed',
+  'InterCompanyVouchRegistrationAllowed',
+  'InvoiceAllowEditExternalExpenseFactor',
+  'InvoiceAllowEditTimeSale',
+  'InvoiceCustomerOrderNumberRequired',
+  'JobCreateAddDebtor',
+  'JobEstimateRequired',
+  'LanguageId',
+  'MaterialCalculationAllowChangeSaleHours',
+  'MaterialGroupId',
+  'MoveActivityLine',
+  'Name',
+  'PaymentTermId',
+  'PlanForegroundColorNumber',
+  'PriceQuoteAddress',
+  'PriceQuoteAllowChangeExternalExpenseFactor',
+  'PriceQuoteAllowChangeTimeSale',
+  'PriceQuoteBlockIfDeviation',
+  'PriceQuoteVatShow',
+  'PriceQuoteVatValue',
+  'ProjectId',
+  'ResponsibleEmployeeId',
+  'ShowActivity',
+  'ShowActivityHours',
+  'ShowActivityLine',
+  'ShowActivityPrice',
+  'ShowPaymentPlan',
+  'ShowPhase',
+  'ShowPhasePrice',
+  'TypeId',
+  'UseFixedDebtorAddressOnPriceQuoteCreation'
+  })
 
 def test_get_costumers():
   costumers = api.get_costumers()
   assert isinstance(costumers, list), "Costumers is a list"
+  for c in costumers:
+    assert COSTUMER_KEYS.issubset(c.keys()), "Costumer has all keys"
 
 
 def test_get_jobs():
@@ -34,3 +107,6 @@ def test_get_employees():
 def test_get_projects():
   projects = api.get_projects()
   assert isinstance(projects, list), "projects is a list"
+  for p in projects:
+    assert PROJECT_KEYS.issubset(p.keys()), "Project has all keys"
+
